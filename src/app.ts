85726e0bfdc4b787e1c52cf6de899cd767513a72
Svitlana Yudina <osbka31@gmail.com>
2023-03-04 17:42:13 +0200

    back to begin

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,17 @@
 import express from 'express';
-// import path from 'path';
+import path from 'path';
 
 const server = express();
 const PORT = process.env.PORT || 8080;
 
-// server.use(express.static(path.resolve('dist')));
+server.use(express.static(path.resolve('dist')));
 
-server.get('/', (req, res) => {
-  // const filePath = path.resolve('dist', 'index.html');
-
-  // res.sendFile(filePath);
+server.get('/main', (req, res) => {
   res.send('<h1>Main page</h1>');
 });
 
 server.get('/about', (req, res) => {
-  res.send('<h1>Hello!</h1>');
+  res.send('<h1>about page</h1>');
 });
 
 server.listen(PORT);
