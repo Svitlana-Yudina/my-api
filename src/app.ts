8464d167ea74da562af5c0da93d2ac9387fc05b7
Svitlana Yudina <osbka31@gmail.com>
2023-03-06 14:41:24 +0200

    try to connect to db

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,43 @@
 /* eslint-disable no-console */
 import express from 'express';
 import path from 'path';
+import pkg from 'pg';
 
 const server = express();
 const port = process.env.PORT || 8080;
+
+const { Client } = pkg;
+// const client = new Client({
+//   host: 'localhost',
+//   user: 'postgres', // owner
+//   password: 'test123',
+//   database: 'test_db', // name of DB
+// });
+
+const client = new Client({
+  host: 'test-app-ydno.onrender.com',
+  user: 'postgres', // owner
+  password: 'test123',
+  database: 'test_db', // name of DB
+});
+
+await client.connect();
+
+const result = await client.query(`
+select *
+from users
+`);
+
+// console.log(result.rows);
 
 server.use(express.static(path.resolve('public')));
 
 server.get('/main', (req, res) => {
   res.send('<h1>Main page</h1>');
+});
+
+server.get('/users', (req, res) => {
+  res.send(result.rows);
 });
 
 server.get('/about', (req, res) => {
